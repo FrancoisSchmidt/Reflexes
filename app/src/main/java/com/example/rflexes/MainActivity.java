a602e67f1cd83d64f35285a52eb1af6cb14fe1af
Franz57 <francois.schmidt@outlook.com>
2020-10-09 11:33:32 +0200

    Améliorations - Mise en place 5 parties puis affichage des 5 scores - Possibilité de redémarrer une partie

--- app/src/main/java/com/example/rflexes/MainActivity.java
+++ app/src/main/java/com/example/rflexes/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.text.Layout;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     public Button catchi;
     public long temps;
     public int tour;
+    public DisplayMetrics metrics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +37,16 @@ public class MainActivity extends AppCompatActivity {
         this.lay.addView(this.catchi);
         this.catchi.setVisibility(View.INVISIBLE);
 
+        this.scores = new ArrayList<Long>();
+
+        this.metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(this.metrics);
 
         this.start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 tour = 0;
+                scores = new ArrayList<Long>();
                 start.setVisibility(View.GONE);
                 startGame();
             }
@@ -51,8 +58,13 @@ public class MainActivity extends AppCompatActivity {
                 temps = System.currentTimeMillis() - starttime;
                 catchi.setVisibility(View.GONE);
                 serie.setText(String.valueOf(temps)+ "ms");
-                if (tour<=5) {
+                scores.add(temps);
+                if (tour<4) {   //tour<(n-1) avec n le nombre de parties souhaitées
+                    tour ++;
                     startGame();
+                }else{
+                    serie.setText(String.valueOf(scores));
+                    start.setVisibility(View.VISIBLE);
                 }
             }
         });
@@ -67,12 +79,14 @@ public class MainActivity extends AppCompatActivity {
                     public void run() {
                         int temps = 1000 + (int) (Math.random() * (5000 - 1000) + 1);
                         try {
-                            Thread.sleep(1000);
+                            Thread.sleep(500);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                         int x = (int) (Math.random() * (200 - 0) + 1);
                         int y = (int) (Math.random() * (200 - 0) + 1);
+                        x =metrics.widthPixels/2;
+                        y=metrics.heightPixels/2;
                         catchi.setX(x);
                         catchi.setY(y);
                         catchi.setVisibility(View.VISIBLE);

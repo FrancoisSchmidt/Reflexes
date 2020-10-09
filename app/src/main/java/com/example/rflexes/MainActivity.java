d695a6ea6fa7cb2eaaa3e06c5fad30e32e782061
Franz57 <francois.schmidt@outlook.com>
2020-10-09 10:32:16 +0200

    Première version qui fonctionne

--- app/src/main/java/com/example/rflexes/MainActivity.java
+++ app/src/main/java/com/example/rflexes/MainActivity.java
@@ -4,15 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     public TextView serie;
     public Button start;
     public ConstraintLayout lay;
+    public long starttime;
+    public ArrayList<Long> scores;
+    public Button catchi;
+    public long temps;
+    public int tour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,37 +31,61 @@ public class MainActivity extends AppCompatActivity {
         this.start = findViewById(R.id.start);
         this.lay = findViewById(R.id.lay);
 
-        Button cacthi = new Button(this);
-        //TODO
-        //Pool -> Garder un seul bouton et récupérer les temps dans des variables
+        this.catchi = new Button(this);
+        this.lay.addView(this.catchi);
+        this.catchi.setVisibility(View.INVISIBLE);
 
 
         this.start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        start.setVisibility(View.GONE);
-                        /*
-                        for (int i=0 ; i<5 ; i++){
-                            int temps = 1000 + (int)(Math.random()*(5000-1000)+1);
-                            int x = (int)(Math.random()*(200-0)+1);
-                            int y = (int)(Math.random()*(200-0)+1);
-                            try {
-                                Thread.sleep(temps);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-
-                        }
-
-                         */
-                    }
-                }).start();
+                tour = 0;
+                start.setVisibility(View.GONE);
+                startGame();
             }
         });
-        
+
+        this.catchi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                temps = System.currentTimeMillis() - starttime;
+                catchi.setVisibility(View.GONE);
+                serie.setText(String.valueOf(temps)+ "ms");
+                if (tour<=5) {
+                    startGame();
+                }
+            }
+        });
+    }
+
+
+    public void startGame(){
+        new Thread() {
+            public void run(){
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        int temps = 1000 + (int) (Math.random() * (5000 - 1000) + 1);
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        int x = (int) (Math.random() * (200 - 0) + 1);
+                        int y = (int) (Math.random() * (200 - 0) + 1);
+                        catchi.setX(x);
+                        catchi.setY(y);
+                        catchi.setVisibility(View.VISIBLE);
+                        starttime = System.currentTimeMillis();
+                    }
+                });
+            }
+        }.start();
+
+
+
+
 
     }
+
 }

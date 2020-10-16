be43dc08ea88f3e6cf373909df5950199ab5aade
Franz57 <francois.schmidt@outlook.com>
2020-10-16 13:32:52 +0200

    Ajout d'une action bar

--- app/src/main/java/com/example/rflexes/MainActivity2.java
+++ app/src/main/java/com/example/rflexes/MainActivity2.java
@@ -1,10 +1,14 @@
 package com.example.rflexes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -27,8 +31,34 @@ public class MainActivity2 extends AppCompatActivity {
         this.text_moy = findViewById(R.id.text_moy);
         this.text_records = findViewById(R.id.text_top);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
         load_moyennes(this);
         load_records(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast.makeText(this, "Failing reset", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
